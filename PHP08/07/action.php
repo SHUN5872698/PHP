b41c96ee49a91dfacae11a6305510a58e542218b
SHUN5872698 <tekisasu.bager@gmail.com>
2021-04-03 11:30:43 +0900

    タスク完了時は項目名と日付に打ち消し線が入るように変更

---
+++ PHP08/07/action.php
@@ -0,0 +1,38 @@
+<?php
+
+//データベースに接続
+$dsn = 'mysql:dbname=php_work2;host=localhost;charset=utf8';
+
+// PDOクラスのインスタンスを作る
+// 引数は、上記のDSN、データベースのユーザー名、パスワード
+$dbh = new PDO($dsn, 'root', 'root');
+
+// $_POST['delete']に1の値が送信されてきていた場合
+if (isset($_POST['delete']) && $_POST['delete'] == "1") {
+
+  //該当レコードの削除を実行するsqlの発行
+  $sql = "DELETE FROM todo_items WHERE id=:id";
+
+  // SQL文を実行する準備
+  $stmt = $dbh->prepare($sql);
+
+  // SQL文の該当箇所に、変数の値を割り当て（バインド）
+  $stmt->bindValue(':id', $_POST['id'], PDO::PARAM_INT);
+} else {
+
+  //そうでない場合該当レコードの完了、未完了を更新するためのsqlを発行
+  $sql = "UPDATE todo_items set is_completed=:is_completed where id=:id";
+
+  // SQL文を実行する準備
+  $stmt = $dbh->prepare($sql);
+
+  // SQL文の該当箇所に、変数の値を割り当て（バインド）
+  $stmt->bindValue(':id', $_POST['id'], PDO::PARAM_INT);
+  $stmt->bindValue(':is_completed', $_POST['is_completed'], PDO::PARAM_INT);
+}
+// SQLを実行
+$stmt->execute();
+
+// 処理が完了したらトップページ（index.php）へリダイレクト
+header('Location: ./');
+exit;
