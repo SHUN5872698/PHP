eabdcb111ed590ec1834049ab8ec6855b33ae9fd
SHUN5872698 <tekisasu.bager@gmail.com>
2021-04-15 12:58:14 +0900

    ユーザ登録と失敗時にエラーメッセージを表示する（登録完了ページは未作成）

---
+++ PHP13/04/download.php
@@ -0,0 +1,32 @@
+<?php
+
+// データベース接続のためのファイルを読み込む
+require_once('./class/db/Base.php');
+require_once('./class/db/TodoItems.php');
+
+try {
+  //インスタンスの生成
+  $db = new TodoItems();
+  // レコードを全件取得する（期限日の古いものから並び替える）
+  $lists = $db->selectAll();
+} catch (Exception $e) {
+  // 例外が発生した場合$eに代入されたインスタンスのメソッドを出力して終了
+  var_dump($e);
+  exit;
+}
+
+// 取得したレコードをCSVファイルとしてダウンロードさせる
+header('Content-Type: text/csv');
+header('Content-Disposition: attachment; filename="download.csv"');
+
+foreach ($lists as $list) {
+  foreach ($list as $key => $val) {
+    // 配列のキーがtodo_itemのとき
+    if ($key == 'todo_item') {
+      // 文字コードをSJISからUTF-8に変換する
+      $list[$key] = mb_convert_encoding($val, 'SJIS-win', 'UTF-8');
+    }
+  }
+  // 配列を「,」で結合して出力する
+  echo implode(',', $list) . "\n";
+}
